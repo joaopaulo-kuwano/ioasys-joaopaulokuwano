372dd916ec31eb052a866a6a2de855ad1c993e5f
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-01-12 18:13:19 -0300

    feat: centraliza modal e adiciona backdrop

--- src/pages/Home/Detail.tsx
+++ src/pages/Home/Detail.tsx
@@ -1,19 +1,22 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 import { Storage } from '../../libs/storage';
 import { IBook, MockBook } from '../../models/Books';
 import { SDK } from '../../sdk';
+import { BookDetail } from './BookDetail';
 
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 ReactModal.setAppElement('#modalAppElement');
@@ -42,10 +45,18 @@ export function Detail({ id }: Props) {
     <div>
       <ReactModal
         isOpen={isOpen}
-        contentLabel="Minimal Modal Example"
+        contentLabel="Detalhe de Livro"
+        style={customStyles}
       >
-        <button type="button" onClick={() => setIsOpen(false)}>Close Modal</button>
-        <span>{book.title}</span>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <i
+            role="log"
+            onClick={() => setIsOpen(false)}
+            className="fas fa-times fa-lg"
+            style={{ color: '#999', cursor: 'pointer' }}
+          />
+        </div>
+        <BookDetail book={book} />
       </ReactModal>
     </div>
   );
