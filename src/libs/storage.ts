4805d033f899c1bd19f4f44d4552b8864ecdd155
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-01-12 16:19:59 -0300

    feat: biblioteca para storage

---
+++ src/libs/storage.ts
@@ -0,0 +1,11 @@
+export type StorageItems = 'authorization' | 'refresh-token' | 'user';
+
+export class Storage {
+  static getItem(key: StorageItems): string {
+    return localStorage.getItem(key) || '';
+  }
+
+  static setItem(key: StorageItems, data: string): void {
+    localStorage.setItem(key, data);
+  }
+}
