a4d9bc3f3bcb4c4b810ac88df15b56f854b5e647
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-01-12 13:16:04 -0300

    feat: geração de rotas com router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/Home';
+import { LoginPage } from './pages/Login';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 import './index.css';
 import { LoginPage } from './pages/Login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LoginPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
