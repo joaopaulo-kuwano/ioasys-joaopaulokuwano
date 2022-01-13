b4c3adc6077e094c33f392e28c258bf43ebf55d8
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-01-12 23:27:01 -0300

    feat: icone e funcao de logout em header

--- src/pages/Home/header.tsx
+++ src/pages/Home/header.tsx
@@ -1,9 +1,21 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import logoImage from '../../assets/logo-preto.png';
+import { Storage } from '../../libs/storage';
 
 export function Header() {
-  const storage = JSON.parse(localStorage.getItem('user') || '{}');
+  const navigate = useNavigate();
+  const user = JSON.parse(Storage.getItem('user') || '{}');
+
+  const logout = () => {
+    Storage.removeItem('user');
+    Storage.removeItem('authorization');
+    Storage.removeItem('refresh-token');
+    navigate('/');
+  };
 
   return (
     // eslint-disable-next-line no-use-before-define
@@ -13,7 +25,11 @@ export function Header() {
         <span className="subtitle">Books</span>
       </div>
       <div className="right">
-        <span className="user-name">{storage.name}</span>
+        <div className="user-name">
+          <span>Bem Vindo (a), </span>
+          <span className="bold">{user.name}</span>
+        </div>
+        <i className="fas fa-sign-out-alt fa-lg icon" onClick={logout} />
       </div>
     </Container>
   );
@@ -23,9 +39,37 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
 
   @media screen and (max-width: 576px) {
     .user-name { display: none };
+  }
+
+  .user-name {
+    font-size: 0.9rem;
+  };
+
+  .left {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .right {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .icon {
+    cursor: pointer;
   }
 
 `;
